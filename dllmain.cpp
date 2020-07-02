257d512b867739b9c17a74e45b64029cb87ed213
playday3008 <titanv3585@gmail.com>
2020-07-02 18:55:24 +0200

    Добавьте файлы проекта.

---
+++ dllmain.cpp
@@ -0,0 +1,29 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) void BypassLLI(int pid)
+{
+    HANDLE csgoProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    LPVOID ntOpenFile = GetProcAddress(LoadLibraryW(L"ntdll"), "NtOpenFile");
+    if (ntOpenFile) {
+        char originalBytes[5];
+        memcpy(originalBytes, ntOpenFile, 5);
+        WriteProcessMemory(csgoProcessHandle, ntOpenFile, originalBytes, 5, NULL);
+    }
+}
